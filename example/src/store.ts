9a2acc950592c899c3bfe7f29f5a9e1d90f77a10
Taras Batenkov <enkot.b@advantagemedia.agency>
2018-07-23 23:36:24 +0300

    added schemas

--- example/src/store.ts
+++ example/src/store.ts
@@ -1,16 +1,19 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import normalizator, { fetch } from '../../src';
+import { user, comment, article } from './schemas';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-
-  },
-  mutations: {
-
-  },
-  actions: {
-
-  },
+  state: {},
+  mutations: {},
+  actions: {},
+  plugins: [
+    normalizator({
+      users: user,
+      comments: comment,
+      articles: article,
+    }),
+  ],
 });
